6f65e5886a05924826cd50180a4b9f8fcb16d799
the-shy-dev <theshydev@gmail.com>
2025-05-13 15:32:16 +0900

    import

---
+++ basics_4.rb
@@ -0,0 +1,21 @@
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+
+my_password.version              #=> "2a"
+my_password.cost                 #=> 12
+puts my_password == "my password"     #=> true
+puts my_password == "not my password" #=> false
+
+my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
+puts my_password == "my password"     #=> true
+puts my_password == "not my password" #=> false
+
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
