d30e2d0c9e35622029544ab6799bab793c3454ba
the-shy-dev <theshydev@gmail.com>
2025-05-21 13:57:06 +0900

    resources and _path

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#indexa"
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles # maps all of the conventional routes for a collection of resources, such as articles
 end
