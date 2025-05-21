d0fbe7aacb2219f59a3bccd1cd5efdb71a31aec5
the-shy-dev <theshydev@gmail.com>
2025-05-21 14:07:36 +0900

    Getting Started - 7.3.1

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
+
+
+# redirect_to will cause the browser to make a new request,
+# whereas render renders the specified view for the current request.
+# It is important to use redirect_to after mutating the database or application state.
+# Otherwise, if the user refreshes the page, the browser will make the
+# same request, and the mutation will be repeated
